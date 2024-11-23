076058bfbefaa1cd2c4dad2a734cca1718150365
Antoine Mairesse <antoine.mairesse@free.fr>
2024-11-23 18:36:43 +0100

    Ajout accès à l'API à partir de la home de l'utilisateur

--- hotel_california/api/urls.py
+++ hotel_california/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RestaurantListAPIView, ClientListAPIView
 
+app_name = 'api'
+
 urlpatterns = [
     path('restaurants/', RestaurantListAPIView.as_view(), name='restaurant-list'),
     path('clients/', ClientListAPIView.as_view(), name='client-list'),
--- hotel_california/hotel_california_app/urls.py
+++ hotel_california/hotel_california_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
+app_name = 'hotel_california_app'
+
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('accounts/login/', LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='hotel_california_app:home'), name='logout'),
     path('setup/', views.setup_view, name='setup_view'),
     path('reset/', views.reset_view, name='reset_view'),
 ]
