6d1a3bec56c50f4592f7f63344a1edfaa396124e
Antoine Mairesse <antoine.mairesse@free.fr>
2024-11-23 18:20:15 +0100

    Fix logout

--- hotel_california/hotel_california_app/urls.py
+++ hotel_california/hotel_california_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('login/', LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('accounts/login/', LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('setup/', views.setup_view, name='setup_view'),
     path('reset/', views.reset_view, name='reset_view'),
 ]
