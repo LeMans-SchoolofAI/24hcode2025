b9f22a56cc6bbe39123fb260840ce14b1f344ec0
Antoine Mairesse <antoine.mairesse@free.fr>
2024-12-07 23:25:00 +0100

    Ajout d'une page de description fictive

--- hotel_california/hotel_california_app/urls.py
+++ hotel_california/hotel_california_app/urls.py
@@ -6,6 +6,7 @@ app_name = 'hotel_california_app'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('description/', views.description_view, name='description'),
     path('accounts/login/', LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
     path('logout/', LogoutView.as_view(next_page='hotel_california_app:home'), name='logout'),
     path('setup/', views.setup_view, name='setup_view'),
