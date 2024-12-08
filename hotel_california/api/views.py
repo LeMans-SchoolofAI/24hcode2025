332217395194922560b2b17e12d5fb029920b974
Antoine Mairesse <antoine.mairesse@free.fr>
2024-12-08 13:40:52 +0100

    Fix sur la création de client

--- hotel_california/api/views.py
+++ hotel_california/api/views.py
@@ -55,6 +55,9 @@ class ClientAPIView(generics.ListCreateAPIView):
     def get(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
     
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     @extend_schema(
         summary="Créer un client",
         description="Créer un client",
